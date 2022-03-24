a78f27badb5e5dd7338cc009be6eb368f62b9d1d
andromarket <jamalelkhdadi@gmail.com>
2022-03-24 17:16:30 +0100

    hshs

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
+//const history = require("connect-history-api-fallback");
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//app.use(history());
+
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.use(express.static("./public/"));
+app.get("/*", (req, res) => res.sendFile("index", { root: "/views/" }));
+
+app.listen(process.env.PORT || 8080);
